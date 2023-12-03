8e34d2485a08f264b3b6b6ac0ca5cff6ac7ebf82
Patrick D'Cruze <patrick.dcruze@neorise.com.au>
2023-12-03 12:11:28 +0800

    Update our apm conversation

--- src/axolotl/prompters.py
+++ src/axolotl/prompters.py
@@ -15,10 +15,10 @@ REPR_TEMPLATE = "\n<start>\n" + Fore.CYAN + "{full_prompt}" + Fore.RESET + "\n<e
 
 register_conv_template(
     Conversation(
-        name="mistral_apm",
+        name="apm",
         system_template="<classifier_prompt>{system_message}",
         system_message="You are a helpful assistant tasked with labelling english messages",
-        roles=["<classifier_input>", "<classifier_output>"],
+        roles=["<classifier_input>", "<classifier_label>"],
         sep_style=SeparatorStyle.NO_COLON_TWO,
         sep="",
         sep2="</s>",
